827d586ea2b8e3f9b608c2766503dc9f2c1420ce
adijha <mail@adijha.com>
2020-05-28 18:30:55 +0530

    header for signin done

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,14 +1,45 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
-const Header = () => {
+const Header = ({title, route, navigation, rightText, rightIcon, noBack}) => {
   return (
-    <View>
-      <Text>v</Text>
+    <View style={styles.container}>
+      <View style={styles.backCover}>
+        {!noBack ? <Text style={styles.rightText}>{rightText}</Text> : null}
+      </View>
+      <Text style={styles.title}>{title}</Text>
+      <View style={styles.rightCover}>
+        {rightText ? <Text style={styles.rightText}>{rightText}</Text> : null}
+      </View>
     </View>
   );
 };
 
 export default Header;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#F45245',
+    height: 60,
+    flexDirection: 'row',
+  },
+  title: {
+    color: '#ffffff',
+    fontSize: 20,
+    textAlign: 'center',
+    marginTop: 16,
+    flex: 1,
+  },
+  rightText: {
+    fontSize: 16,
+    marginTop: 16,
+    marginRight: 15,
+    color: '#ffffff',
+  },
+  rightCover: {
+    width: 50,
+  },
+  backCover: {
+    width: 50,
+  },
+});
