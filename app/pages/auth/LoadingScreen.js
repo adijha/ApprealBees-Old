22c28b903db656ceb06156d7ca4ec3fdd5390a41
adijha <mail@adijha.com>
2020-05-29 00:46:52 +0530

    done

--- app/pages/auth/LoadingScreen.js
+++ app/pages/auth/LoadingScreen.js
@@ -6,7 +6,7 @@ export default function LoadingScreen(props) {
   const tryLogin = useCallback(async () => {
     setTimeout(async () => {
       const token = await AsyncStorage.getItem('token');
-      // props.navigation.navigate(token ? 'Home' : 'SignIn');
+      props.navigation.navigate(token ? 'Home' : 'SignIn');
     }, 1500);
   }, [props.navigation]);
   useEffect(() => {
