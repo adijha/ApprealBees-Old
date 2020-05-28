00e286ecbb4ce57ceded98642252f21a100048e4
adijha <mail@adijha.com>
2020-05-28 23:40:51 +0530

    All done

--- app/pages/home/HomeScreen.js
+++ app/pages/home/HomeScreen.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet, Text, View, Button} from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
+import Header from '../../components/Header';
 
 const HomeScreen = props => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  useEffect(() => {
+    checkLogin();
+  });
+  const checkLogin = async () => {
+    let token = AsyncStorage.getItem('token');
+    if (token) {
+      setLoggedIn(true);
+    }
+  };
+
+  props.navigation.setOptions({
+    header: () => (
+      <Header
+        route={props.route}
+        navigation={props.navigation}
+        title="Shop"
+        noBack
+      />
+    ),
+  });
+
   const logOut = async () => {
     await AsyncStorage.removeItem('token');
     props.navigation.navigate('Auth');
@@ -10,8 +33,10 @@ const HomeScreen = props => {
 
   return (
     <View>
-      <Text>HomeScreen HomeScreen</Text>
-      <Button title="Log Out" onPress={logOut} />
+      <Text style={{textAlign: 'center', marginVertical: 50, fontSize: 20}}>
+        This is home Screen
+      </Text>
+      {loggedIn ? <Button title="Log Out" onPress={logOut} /> : null}
     </View>
   );
 };
