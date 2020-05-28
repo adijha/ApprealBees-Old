19384c8bfe1d5da956301042d396664213ab21fc
adijha <mail@adijha.com>
2020-05-28 17:30:11 +0530

    nav setup

---
+++ app/navigation/mainNavigator.js
@@ -0,0 +1,45 @@
+import React from 'react';
+//navigation imports
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+//auth
+import LoadingScreen from '../pages/auth/LoadingScreen';
+import SignInScreen from '../pages/auth/SignInScreen';
+import SignUpScreen from '../pages/auth/SignUpScreen';
+//Home
+import HomeScreen from '../pages/home/HomeScreen';
+// initialize navigator
+const Stack = createStackNavigator();
+
+export const AuthNavigator = () => (
+  <Stack.Navigator screenOptions={{headerShown: false}}>
+    <Stack.Screen name="Loading" component={LoadingScreen} />
+    <Stack.Screen name="SignIn" component={SignInScreen} />
+    <Stack.Screen name="SignUp" component={SignUpScreen} />
+  </Stack.Navigator>
+);
+
+export const HomeNavigator = () => (
+  <Stack.Navigator screenOptions={{headerShown: false}}>
+    <Stack.Screen name="Home" component={HomeScreen} />
+  </Stack.Navigator>
+);
+
+const MainContainer = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          options={() => ({
+            headerShown: false,
+          })}
+          name="Auth"
+          component={AuthNavigator}
+        />
+        <Stack.Screen name="Earn" component={HomeNavigator} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default MainContainer;
